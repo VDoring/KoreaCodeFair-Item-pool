01ad7032484e8dafdd13ccae33211bb01fe6aa5a
Doring <hojun.dev@gmail.com>
2019-07-30 10:33:19 +0900

    문자열 갯수 세기
    strlen 사용

---
+++ 구름IDE-C/함수/strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	char arr[31];
+
+	scanf("%s", arr);
+	printf("%d", strlen(arr));
+
+	return 0;
+
+}
+
+// https://koreacodefair.kr/learn/lecture/14776/%EB%B0%94%EB%A1%9C-%EC%8B%A4%ED%96%89%ED%95%B4%EB%B3%B4%EB%A9%B4%EC%84%9C-%EB%B0%B0%EC%9A%B0%EB%8A%94-c%EC%96%B8%EC%96%B4/lesson/681030/%EB%AC%B8%EC%9E%90%EC%97%B4-%EA%B0%AF%EC%88%98-%EC%84%B8%EA%B8%B0
