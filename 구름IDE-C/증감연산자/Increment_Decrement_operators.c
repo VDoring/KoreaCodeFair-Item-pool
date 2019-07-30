ab516bc014877a4074b0de0363c0c7d02441f7b4
Doring <hojun.dev@gmail.com>
2019-07-30 10:31:03 +0900

    증감연산자
    a++, ++a, a--, --a의 차이를 알수 있게 한다

---
+++ 구름IDE-C/증감연산자/Increment_Decrement_operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	int num = 3;
+
+	printf("%d\n", num++);
+	printf("%d\n", ++num);
+	printf("%d\n", num--);
+	printf("%d\n", --num);
+
+	return 0;
+}
+
+// (증감연산자)
+// https://koreacodefair.kr/learn/lecture/14776/%EB%B0%94%EB%A1%9C-%EC%8B%A4%ED%96%89%ED%95%B4%EB%B3%B4%EB%A9%B4%EC%84%9C-%EB%B0%B0%EC%9A%B0%EB%8A%94-c%EC%96%B8%EC%96%B4/lesson/680958/%EC%A6%9D%EA%B0%90-%EC%97%B0%EC%82%B0%EC%9E%90
